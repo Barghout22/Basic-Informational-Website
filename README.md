2df06f622254652a768c11456bd6745cb97cfe68
Mahmoud <barghoutmahmoud2@gmail.com>
2023-04-17 15:49:55 +0200

    created the basic four files required and had them displayed from the server based on the required url

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Basic-Informational-Website
+
+created basic html file reads and displays using a simple node server.
---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+
+http
+  .createServer(function (req, res) {
+    if (req.url === "/") {
+      fs.readFile("index.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else if (req.url === "/about") {
+      fs.readFile("about.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else if (req.url === "/contact") {
+      fs.readFile("contact.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile("404.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    }
+  })
+  .listen(8080, () => {
+    console.log("listeningg on port 8080");
+  });
